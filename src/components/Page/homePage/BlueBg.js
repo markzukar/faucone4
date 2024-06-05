1461e90d3215fa601fa105715e5fc9109afb26a7
AV_Prakash <prakash@jobsgaar.com>
2024-06-05 16:52:45 +0530

    new home changes

--- src/components/Page/homePage/BlueBg.js
+++ src/components/Page/homePage/BlueBg.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react'
-import zoho_bg from '../../../img/zoho_bg.PNG'
+import bluebg from '../../../img/bluebg.png'
 import $ from 'jquery';
 
 
@@ -44,9 +44,9 @@ const BlueBg = () => {
     return (
         <>
             {/*  */}
-            <main className='tw-bg-[#0060b4] tw-text-white tw-py-16 md:tw-px-36 lg:tw-p-0 lg:tw-pt-24 lg:tw-flex tw-items-end tw-rounded-3xl tw-border-solid tw-border-2 tw-mr-20 tw-ml-20 tw-mt-7' onMouseEnter={() => Counterup()}>
+            <main className='tw-bg-[#059fce] tw-text-white tw-py-16 md:tw-px-36 lg:tw-p-0 lg:tw-pt-24 lg:tw-flex tw-items-end tw-rounded-3xl tw-border-solid tw-border-2 tw-mr-20 tw-ml-20 tw-mt-7' onMouseEnter={() => Counterup()}>
 
-                <section className='lg:tw-w-[70%] lg:tw-pl-20 lg:tw-pb-44 xl:tw-pl-36 tw-font-PuviMedium'>
+                <section className='lg:tw-w-[80%] lg:tw-pl-20 lg:tw-pb-44 xl:tw-pl-36 tw-font-PuviMedium'>
                     <p className='tw-text-center lg:tw-text-left tw-text-3xl lg:tw-font-base lg:tw-text-6xl tw-font-medium'>Made for the <strong>Global</strong></p>
                     {/* <p className='tw-text-center lg:tw-text-left tw-text-3xl lg:tw-text-5xl tw-font-medium lg:tw-font-normal '>Made for the World.</p> */}
 
@@ -54,27 +54,27 @@ const BlueBg = () => {
                     <div className='sm:tw-grid tw-grid-cols-2 sm:tw-mt-10 '>
                         <div className='tw-text-center tw-mt-14 sm:tw-mt-5 lg:tw-flex lg:tw-items-center '>
                             <p className='tw-text-3xl lg:tw-text-left tw-font-medium tw-tracking-wide lg:tw-w-32 '><span id="counter">250</span>+</p>
-                            <p className='  lg:tw-text-left lg:tw-border-b tw-font-bold'>Services</p>
+                            <p className='  lg:tw-text-left lg:tw-border-b tw-text-2xl'>Services</p>
                         </div>
 
                         <div className='tw-text-center lg:tw-text-left tw-mt-14 sm:tw-mt-5 lg:tw-flex lg:tw-items-center '>
                             <p className='tw-text-3xl tw-font-medium tw-tracking-wide lg:tw-w-32 '><span id="counter1">15</span>+</p>
-                            <p className='  lg:tw-text-left lg:tw-border-b tw-font-bold'>Years in Business</p>
+                            <p className='  lg:tw-text-left lg:tw-border-b tw-text-2xl'>Years in Business</p>
                         </div>
 
                         <div className='tw-text-center lg:tw-text-left tw-mt-14 sm:tw-mt-5 lg:tw-flex lg:tw-items-center '>
                             <p className='tw-text-3xl tw-font-medium tw-tracking-wide lg:tw-w-32 '><span id="counter2">12</span>+</p>
-                            <p className='  lg:tw-text-left lg:tw-border-b tw-font-bold'>Countries Served</p>
+                            <p className='  lg:tw-text-left lg:tw-border-b tw-text-2xl'>Countries Served</p>
                         </div>
 
                         <div className='tw-text-center lg:tw-text-left tw-mt-14 sm:tw-mt-5 lg:tw-flex lg:tw-items-center '>
                             <p className='tw-text-3xl tw-font-medium tw-tracking-wide lg:tw-w-32 '><span id="counter3">150</span>+</p>
-                            <p className='  lg:tw-text-left lg:tw-border-b tw-font-bold'>Support Centers Globally</p>
+                            <p className='  lg:tw-text-left lg:tw-border-b tw-text-2xl'>Support Centers Globally</p>
                         </div>
 
                         <div className='tw-text-center lg:tw-text-left tw-mt-14 sm:tw-mt-5 lg:tw-flex lg:tw-items-center '>
                             <p className='tw-text-3xl tw-font-medium tw-tracking-wide lg:tw-w-32 '><span id="counter4">20</span>+</p>
-                            <p className='  lg:tw-text-left lg:tw-border-b tw-font-bold'>Unique Solutions</p>
+                            <p className='  lg:tw-text-left lg:tw-border-b tw-text-2xl'>Unique Solutions</p>
                         </div>
 
                         {/* <div className='tw-flex tw-justify-center lg:tw-justify-start tw-items-center '>
@@ -85,9 +85,9 @@ const BlueBg = () => {
                 </section>
 
 
-                {/* <section className='md:tw-flex tw-justify-center tw-hidden lg:tw-inline  '>
-                    <img src={zoho_bg} alt="" className='tw-w-[70%] lg:tw-w-auto ' />
-                </section> */}
+                <section className='md:tw-flex tw-justify-center tw-hidden lg:tw-inline tw-mr-24 tw-mb-6 '>
+                    <img src={bluebg} alt="" className='tw-w-[570px] tw-mx-auto ' />
+                </section>
             </main>
         </>
     )
--- src/components/Page/homePage/CustomerSupport.js
+++ src/components/Page/homePage/CustomerSupport.js
@@ -1,25 +1,54 @@
 import React from 'react'
-import brand from '../../../img/brand.png'
+import customersupport from '../../../img/customersupport.png'
+import ladybackground from '../../../img/ladybackground.png';
+import customerIcon from '../../../img/support1.png'
+import experience from '../../../img/experience.png'
+import technology from '../../../img/technology.png'
 
 const CustomerSupport = () => {
     return (
         <>
-            <section className='tw-flex tw-items-center tw-justify-center tw-mt-5 tw-ml-20 tw-mr-20 tw-bg-[#0baad4] tw-font-PuviMedium tw-rounded-3xl tw-border-solid tw-border-2 '>
+            <section className='tw-flex tw-p-10 tw-items-center tw-justify-center tw-mt-5 tw-ml-20 tw-mr-20 tw-bg-[#0baad4] tw-font-PuviMedium tw-rounded-3xl tw-border-solid tw-border-2 '>
 
 
-                <div className='tw-w-[50%] '>
-                    <img src={brand} alt="" className='tw-w-[500px] tw-mx-auto' />
+                <div className='tw-w-[50%]' style={{ "backgroundImage": `url(${ladybackground})` }}>
+                    <img src={customersupport} alt="" className='tw-w-[300px]  tw-mx-auto' />
                 </div>
 
-                <div className=' tw-text-white tw-w-[40%] ' >
-                    <p className='tw-font-bold tw-mt-10 tw-text-4xl'>Customer support centre</p>
-                    <p className=' tw-mt-3 tw-text-base'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed qui consequuntur magni dolores eosqui ratione voluptatem sequi nesciunt orro est, qu dolorem ipsum quia dolor sit amet, consectetur, adipisci velittotam rem aperiam eaque quae inventore veritatis beatae vitae dicta sunt explicabo.
-                    </p>
+                <div className=' tw-text-white tw-text-2xl tw-w-[40%] ' >
 
-                    <p className='tw-font-bold tw-mt-7 tw-text-4xl'>Customer experience centre</p>
-                    <p className='tw-mt-3 tw-text-base'>Aquia non numquam eius modi incidunt ut labore aliquam quaerat voluptatem nim  ad minima veniam, quis nostrum ullam corporis suscipit laboriosam, nisi ut aliquid ex  ea commodi consequatur
-                    </p>
+                    <div class="tw-flex tw-h-24 tw-items-center">
+                        <div class="tw-flex-none tw-w-14 tw-content-center">
+                            <img src={customerIcon} className="tw-h-14 tw-w-20" />
+                        </div>
+                        <div class="tw-flex-initial">
+                            <p className='tw-pl-6 '>Customer support centre</p>
+                        </div>
+                    </div>
+                    <div class="tw-flex tw-h-24 tw-items-center">
+                        <div class="tw-flex-none tw-w-14 tw-content-center">
+                            <img src={experience} className="tw-h-14 tw-w-20" />
+                        </div>
+                        <div class="tw-flex-initial">
+                            <p className='tw-pl-6 '>Customer experience centre</p>
+                        </div>
+                    </div>
+                    <div class="tw-flex tw-h-24 tw-items-center">
+                        <div class="tw-flex-none tw-w-14 tw-content-center">
+                            <img src={technology} className="tw-h-14 tw-w-20" />
+                        </div>
+                        <div class="tw-flex-initial ">
+                            <p className='tw-pl-6'>Technology Development centre</p>
+                        </div>
+                    </div>
+
+                    {/* <p className='tw-mt-5 '>Customer support centre</p>
+                    <p className='tw-mt-5 '>Customer experience centre</p>
+                    <p className='tw-mt-5 '>Technology Development centre</p> */}
                 </div>
+
+
+
 
 
             </section>
